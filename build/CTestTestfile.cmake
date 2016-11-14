df0a40be754a9b688266f39075f3b91578d01fe4
Sergi <sergi@sergi-Satellite-A660>
2016-11-14 14:36:45 +0100

    Create the first robot 2R

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sergi/Cam2R_ws/src
+# Build directory: /home/sergi/Cam2R_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(Cam2R)
